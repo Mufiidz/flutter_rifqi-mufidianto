99ba18d84049f91434dd4f646bff3d799a3606fc
Mufidz <rmufidianto@outlook.com>
2022-03-05 10:04:29 +0700

    update task section 07 & 08

--- 08_Collection/praktikum/task_02.dart
+++ 08_Collection/praktikum/task_02.dart
@@ -1,40 +1,22 @@
-import 'dart:math';
-
 void main() {
-  var map = getMap();
-  String hasil = '''Terdapat sebuah map sebagai berikut:
-  $map\n----------------------------------------------\nKetika di iterasi map tersebut menjadi:''';
-
-  print(hasil);
-
-  for (var key in map.keys) {
-    print("$key -> ${map[key]}");
-  }
-}
-
-int randomRange(int min, int max) => min + Random().nextInt(max - min);
-
-List<int> getListNumber() {
-  List<int> nums = [];
-  for (var i = 1; i <= randomRange(3, 10); i++) {
-      int num = Random().nextInt(100);
-      nums.add(num);
-    }
-  return nums;
-}
-
-List<dynamic> getMainList() {
-  var listNums = [];
-  for (var i = 0; i < 2; i++) {
-    listNums.add(getListNumber());
-  }
-  return listNums;
-}
-
-Map<dynamic, dynamic> getMap() {
   var maps = {};
-  for (var i = 1; i <= randomRange(2, 5); i++) {
-    maps['list$i'] = getMainList();
+  var list = [
+    ["Nama", "Fulan"],
+    ["Asal", "Jakarta"],
+    ["No.Telp", "0987654321"]
+  ];
+
+  for (var item in list) {
+    maps[item[0]] = item[1];
   }
-  return maps;
+
+  print('''
+  Terdapat sebuah list sebagai berikut : 
+    $list
+  Dan berikut dirubah dalam bentuk map :
+    $maps''');
+
+  for (var item in maps.keys) {
+    print(maps[item]);
+  }  
 }
